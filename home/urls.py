6868632dc2c2be76111dedd36b4465ed8b0b0aa6
= <shashwatthegamer332@gmail.com>
2024-07-11 15:51:07 +0530

    raw data view added

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, raw
 
 urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
+    path('raw/', raw, name='raw'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
+from myapp.models import DataDump 
+
 
 # Create your views here.
 def home(request):
 
     if request.user.is_authenticated:
         return render(request, 'home.html')
+    else:
+        return redirect('login')
+
+def raw(request):
+    data = DataDump.objects.all()
+    if request.user.is_authenticated:
+        return render(request, 'raw.html', {'data': data})
     else:
         return redirect('login')
