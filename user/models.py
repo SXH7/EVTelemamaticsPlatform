a8b347604ad319b713b310985892e8eac597fb10
Shashwat Singh <shashwatthegamer332@gmail.com>
2024-09-15 21:18:38 +0530

    test

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib import admin
 
-
+# dhruv ghaziabadi
 
 # Create your models here.
 class User(AbstractUser):
